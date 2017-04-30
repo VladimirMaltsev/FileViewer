6b3bda859c35ab8a09bdf9f3264ddc521efce2bc
Maltsev Vladimir <st054439@student.spbu.ru>
2017-04-30 21:50:06 +0300

    add select item for bmp8

--- Controller/AppController.kt
+++ Controller/AppController.kt
@@ -2,6 +2,7 @@ package Controller
 
 import Model.Bmp24Model
 import Model.Bmp32Model
+import Model.Bmp8Model
 import Model.ModelInterface
 import View.ViewInterface
 import java.io.FileInputStream
@@ -25,19 +26,27 @@ class AppController (override var view: ViewInterface,
 
     override fun setModel(data: ByteArray) {
         var type : String = String(data.copyOfRange(0, 2))
-        if (type == "BM")
-            type += data[0x1C]
         print(type)
+
         when (type) {
-            "BM24" -> {
-                    if (model !is Bmp24Model)
-                        model = Bmp24Model()
-            }
-            "BM32" -> {
-                if (model !is Bmp32Model)
-                    model = Bmp32Model()
+            "BM" -> {
+                when (data[0x1C].toInt()) {
+
+                    8 ->
+                        if (model !is Bmp8Model)
+                            model = Bmp8Model()
+
+                    24 ->
+                        if (model !is Bmp24Model)
+                            model = Bmp24Model()
+
+                    32 ->
+                        if (model !is Bmp32Model)
+                            model = Bmp32Model()
+                }
             }
         }
+
         model.registerObserver(view)
         model.inflate(data)
     }
