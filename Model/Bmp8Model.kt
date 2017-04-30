3eec7cd6d9609f3d8dac704b9fbd14e167df9906
Maltsev Vladimir <st054439@student.spbu.ru>
2017-04-30 21:45:20 +0300

    add bmp_8bit support

---
+++ Model/Bmp8Model.kt
@@ -0,0 +1,37 @@
+package Model
+
+import View.Observer
+
+/**
+ * Created by Вдадимир on 30.04.2017.
+ */
+class Bmp8Model : ModelInterface{
+
+
+    override var height: Int = 0
+    override var width: Int = 0
+    override var size: Int = 0
+    override var bytesForPixel: Int = 0
+    override var pixelStartIndex: Int = 0
+    override var countColorsInTable: Int = 0
+    override var pixelArray: ByteArray? = null
+    override var colorTable: ByteArray? = null
+
+    override var observers: ArrayList<Observer> = ArrayList()
+
+    override fun inflate(data: ByteArray) {
+        size = getValue(data, 0x02, 4)
+        width = getValue(data, 0x12, 4)
+        height = getValue(data, 0x16, 4)
+        bytesForPixel = getValue(data, 0x1C, 1)
+        countColorsInTable = getValue(data, 0x2E, 4)
+        pixelStartIndex = getValue(data, 0x0A, 4)
+        pixelArray = data.copyOfRange(pixelStartIndex, size)
+        colorTable = data.copyOfRange(0x36, pixelStartIndex - 1)
+
+        notifyObservers()
+    }
+
+
+
+}
